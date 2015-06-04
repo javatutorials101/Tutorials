8e743af0dbc5a4d548299bfce5f865e462a02c73
javatutorials101 <javatutorials101@gmail.com>
2015-06-04 13:01:42 +0200

    Source code for 3D tutorials

---
+++ 3D - Tutorial 6/Calculator.java
@@ -0,0 +1,20 @@
+
+public class Calculator {
+	static double CalculatePositionX(double[] ViewFrom, double[] ViewTo, double x, double y, double z)
+	{
+		return 0;
+	}
+
+	static double CalculatePositionY(double[] ViewFrom, double[] ViewTo, double x, double y, double z)
+	{
+		setStuff(ViewFrom, ViewTo);
+		return 0;		
+	}
+	
+	static void setStuff(double[] ViewFrom, double[] ViewTo)
+	{
+		Vector ViewVector = new Vector(ViewTo[0] - ViewFrom[0], ViewTo[1] - ViewFrom[1], ViewTo[2] - ViewFrom[2]);
+		
+		
+	}
+}
---
+++ 3D - Tutorial 6/Vector.java
@@ -0,0 +1,10 @@
+
+public class Vector {
+	double x, y, z;
+	public Vector(double x, double y, double z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
